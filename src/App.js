e92194fcd60fafdc211c80507358df3d30677925
stevenwett <stevenwett@gmail.com>
2021-01-30 20:09:56 -0500

    Text edits

--- src/App.js
+++ src/App.js
@@ -15,13 +15,12 @@ function App() {
           <div className="page-content col-sm-12 col-md-10 col-lg-8">
             <h1>Steven Wett</h1>
             <p className="h2">Front-end Web Developer</p>
-            <p>This site was created by me, Steven Wett, and showcases all original photos. I'm a front-end developer who is passionate about human-centric design and accessibility. In addition to coding, I also am a photographer, enjoy gardening, fermenting things, and generally being outdoors.</p>
+            <p>This site was created by me, Steven Wett, and showcases all original photos. I'm a front-end developer who is passionate about human-centric design and accessibility. In addition to coding, I also enjoy taking photos, gardening, fermenting things, and generally being outdoors.</p>
             <div className="contact">
-              <p>If you want to get in touch:</p>
               <ul>
-                <li><a href="mailto:stevenwett@gmail.com">Email</a></li>
-                <li><a href="https://linkedin.com/in/stevenwett/">LinkedIn</a></li>
-                <li><a href="https://github.com/stevenwett/">Github</a></li>
+                <li><a href="mailto:stevenwett@gmail.com">stevenwett@gmail.com</a></li>
+                <li><a href="https://linkedin.com/in/stevenwett/">linkedin.com/in/stevenwett</a></li>
+                <li><a href="https://github.com/stevenwett/">github.com/stevenwett</a></li>
               </ul>
             </div>
             <div className="dog-photos row">
