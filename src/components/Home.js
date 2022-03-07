2a628a960645e27eb2f2083ecf93b95822891660
stevenwett <stevenwett@gmail.com>
2022-03-06 22:07:25 -0500

    Styling updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -118,7 +118,7 @@ class Home extends React.Component {
 					],
 					features: [
 						'Custom graphics',
-						'Jobs listings',
+						'Job listings',
 					],
 					imageSrc: 'plainview-milk.jpg',
 					imageWebpSrc: 'plainview-milk.webp',
@@ -127,7 +127,7 @@ class Home extends React.Component {
 				},
 				{
 					name: 'Portico Healthnet',
-					description: 'A custom WordPress site for Portico Healthnet, a healcare advocacy nonprofit in Minnesota.',
+					description: 'A custom WordPress site for Portico Healthnet, a healthcare advocacy nonprofit in Minnesota.',
 					industries: [
 						'healthcare',
 						'consumer-services',
@@ -268,31 +268,33 @@ class Home extends React.Component {
 			projects = filteredProjectsList.map((project, index) => {
 				return(
 					<article className="project" key={index}>
-						<div className="project-content">
-							<h3>{project.name}</h3>
-							{project.linkPretty
-								? <p className="link"><a href={project.link}>{project.linkPretty}</a></p>
-								: null}
-							<p>{project.description}</p>
-							{project.features.length > 0
-								? (
-									<ul>
-										{project.features.map((item, index) => {
-											return (
-												<li key={index}>{item}</li>
-											)
-										})}
-									</ul>
-								) : null}
-						</div>
-						<div className="project-image">
-							<a className="image" href={project.link}>
-								<picture>
-									<source srcSet={`/images/screenshots/${project.imageWebpSrc}`} type="image/webp" />
-									<img src={`/images/screenshots/${project.imageSrc}`} alt={project.name} />
-								</picture>
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6.188 8.719c.439-.439.926-.801 1.444-1.087 2.887-1.591 6.589-.745 8.445 2.069l-2.246 2.245c-.644-1.469-2.243-2.305-3.834-1.949-.599.134-1.168.433-1.633.898l-4.304 4.306c-1.307 1.307-1.307 3.433 0 4.74 1.307 1.307 3.433 1.307 4.74 0l1.327-1.327c1.207.479 2.501.67 3.779.575l-2.929 2.929c-2.511 2.511-6.582 2.511-9.093 0s-2.511-6.582 0-9.093l4.304-4.306zm6.836-6.836l-2.929 2.929c1.277-.096 2.572.096 3.779.574l1.326-1.326c1.307-1.307 3.433-1.307 4.74 0 1.307 1.307 1.307 3.433 0 4.74l-4.305 4.305c-1.311 1.311-3.44 1.3-4.74 0-.303-.303-.564-.68-.727-1.051l-2.246 2.245c.236.358.481.667.796.982.812.812 1.846 1.417 3.036 1.704 1.542.371 3.194.166 4.613-.617.518-.286 1.005-.648 1.444-1.087l4.304-4.305c2.512-2.511 2.512-6.582.001-9.093-2.511-2.51-6.581-2.51-9.092 0z"/></svg>
-							</a>
+						<div className="inner">
+							<div className="project-content">
+								<h3>{project.name}</h3>
+								{project.linkPretty
+									? <p className="link"><a href={project.link}>{project.linkPretty}</a></p>
+									: null}
+								<p>{project.description}</p>
+								{project.features.length > 0
+									? (
+										<ul>
+											{project.features.map((item, index) => {
+												return (
+													<li key={index}>{item}</li>
+												)
+											})}
+										</ul>
+									) : null}
+							</div>
+							<div className="project-image">
+								<a className="image" href={project.link}>
+									<picture>
+										<source srcSet={`/images/screenshots/${project.imageWebpSrc}`} type="image/webp" />
+										<img src={`/images/screenshots/${project.imageSrc}`} alt={project.name} />
+									</picture>
+									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6.188 8.719c.439-.439.926-.801 1.444-1.087 2.887-1.591 6.589-.745 8.445 2.069l-2.246 2.245c-.644-1.469-2.243-2.305-3.834-1.949-.599.134-1.168.433-1.633.898l-4.304 4.306c-1.307 1.307-1.307 3.433 0 4.74 1.307 1.307 3.433 1.307 4.74 0l1.327-1.327c1.207.479 2.501.67 3.779.575l-2.929 2.929c-2.511 2.511-6.582 2.511-9.093 0s-2.511-6.582 0-9.093l4.304-4.306zm6.836-6.836l-2.929 2.929c1.277-.096 2.572.096 3.779.574l1.326-1.326c1.307-1.307 3.433-1.307 4.74 0 1.307 1.307 1.307 3.433 0 4.74l-4.305 4.305c-1.311 1.311-3.44 1.3-4.74 0-.303-.303-.564-.68-.727-1.051l-2.246 2.245c.236.358.481.667.796.982.812.812 1.846 1.417 3.036 1.704 1.542.371 3.194.166 4.613-.617.518-.286 1.005-.648 1.444-1.087l4.304-4.305c2.512-2.511 2.512-6.582.001-9.093-2.511-2.51-6.581-2.51-9.092 0z"/></svg>
+								</a>
+							</div>
 						</div>
 					</article>
 				)
@@ -310,10 +312,10 @@ class Home extends React.Component {
 				<div className="page-home">
 					<div className="container">
 						<div className="row">
-							<div className="col-12 col-md-10 col-lg-8 col-xl-7">
+							<div className="col-12 col-md-10 col-lg-8 col-xl-6">
 								<div className="intro">
 									<div className="row">
-										<div className="col-12 col-md-10 col-lg-10 col-xl-9">
+										<div className="col-12 col-md-10 col-lg-10">
 											<div className="name-title">
 												<h1>Steven Wett</h1>
 												<div className="title">Web Developer</div>
@@ -363,7 +365,7 @@ class Home extends React.Component {
 										{null !== industryOptionSelected
 											? (getIndustryName(industryOptionSelected)
 												? <p className="results">
-													Showing {projects.length} {resultText} in the industry &ldquo;{getIndustryName(industryOptionSelected)}&rdquo;
+													Showing {projects.length} {resultText} categorized as &ldquo;{getIndustryName(industryOptionSelected)}&rdquo;
 													<button
 														className="clear"
 														onClick={
